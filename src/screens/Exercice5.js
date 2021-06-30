b7f3b3e642a587b469c9d5be91bc7933ffa981c6
Med-Ahmed-Kilani <ahmedkilani890@gmail.com>
2021-06-30 18:31:50 +0100

    ex 5

--- src/screens/Exercice5.js
+++ src/screens/Exercice5.js
@@ -4,14 +4,36 @@ import Header from "../components/Header";
 
 const Exercice5 = ({ navigation }) => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Header nav={navigation} />
-
-      <Text></Text>
+      <View style={styles.container}>
+        <View style={[styles.square, { backgroundColor: "skyblue" }]}>
+          <Text>Square 1</Text>
+        </View>
+        <View style={[{ backgroundColor: "lightgreen" }, styles.square]}>
+          <Text>Square 2</Text>
+        </View>
+        <View style={[styles.square, { backgroundColor: "pink" }]}>
+          <Text>Square 3</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Exercice5;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  square: {
+    height: 100,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+});
